56ef22c34a1abcc48f630965f2f2dcce9422d993
charlie <president_bond@icloud.com>
2020-06-11 16:16:22 -0700

    add logger server

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const {generate} = require("./ASTgenerater");
-const madge = require('madge');
-
-
-
+const fs = require("fs");
+const madge = require('madge'); // use madge to construct file dependency
+const envfile = require('envfile')
 const codeBase = "./code/uppy/"
 
+
+//init
+let parsedEnv = envfile.parse(fs.readFileSync('.env'));
+parsedEnv.SRT_PATH = __dirname;
+fs.writeFileSync("./.env", envfile.stringify(parsedEnv))
+
+require("dotenv").config()
 // generate(codeBase)
 
-madge('./code/uppy').then((res) => {
-	console.log(res)
-});
+console.log("env", process.env.SRT_PATH)
+
+if(!fs.existsSync(process.env.SRT_PATH+'/tmp'))
+    fs.mkdirSync(process.env.SRT_PATH+'/tmp');
+
+// madge('./code/uppy').then((res) => {
+// 	console.log(res)
+// });
---
+++ instrument/server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const fs = require('fs')
+const bodyParser = require("body-parser");
+const path = require('path')
+require("dotenv").config({path: path.resolve(__dirname, "../.env")})
+const app = express()
+const port = 8888
+
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+app.use(bodyParser.json());
+
+
+/*
+  * instrument server side logger
+  * params: req.body.
+  *     fileName: fileName in ../tmp/ folder, create new file if doesn't exist
+  *     msg: message to add to file
+  * 
+  * automatically add \n to each message
+  * remember to use --header 'Content-Type: application/x-www-form-urlencoded'
+*/
+app.post('/instrument-message', (req, res) => {
+    console.log("POST", req.body)
+    let logStream;   
+    try {
+        let log_path = path.join(process.env.SRT_PATH, "tmp", req.body.fileName);
+        logStream = fs.createWriteStream(log_path, {flags: "a"});
+        logStream.write(req.body.msg);
+        logStream.end('\n');
+    } catch(err) {
+        res.status(500).send({error: err});
+    }
+    res.send('ok')
+    
+})
+
+app.listen(port, () => console.log(`app listening at http://localhost:${port}`))
