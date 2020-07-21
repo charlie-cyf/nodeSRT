253652cc5c80d8648fbce8865270882117d96ba9
charlie-cyf <2013oct20@gmail.com>
2020-07-21 23:15:32 +0000

    static analysis on test changes

--- changeAnalysis/index.js
+++ changeAnalysis/index.js
@@ -4,10 +4,19 @@ const path = require('path');
 const fs = require('fs');
 const ASTgenerator = require('../ASTgenerater');
 const globalUtil = require('../util')
+const acornWalk = require('acorn-walk');
+const { extend } = require('acorn-jsx-walk');
+const { default: t } = require('typy');
+
+acornWalk.base.FieldDefinition = (node, st, c) => {
+    if (node.computed) c(node.key, st, "Expression")
+    c(node.value, st, "Expression")
+}
+extend(acornWalk.base);
 
 
 // Compare AST2 to AST1 to output a list of changes' ancestors
-// ! same modification might be pushed twice to diffList, need to check function name to remove dup.
+// ! same modifications might be pushed twice to diffList, need to check function name to remove dup.
 module.exports.getDiffs = function (AST1, AST2) {
     let diffList = [];
 
@@ -55,6 +64,130 @@ module.exports.getDiffs = function (AST1, AST2) {
     return diffList;
 }
 
+function objDeepEquals(obj1, obj2) {
+    if(_.isEqual(obj1, obj2)) return true;
+    if(obj1.type !== obj2.type) return false;
+
+    const temp1 = Object.keys(obj1).filter(k => !['start', 'end', 'comments', 'line', 'column'].includes(k))
+    const temp2 = Object.keys(obj1).filter(k => !['start', 'end', 'comments', 'line', 'column'].includes(k))
+
+    if(temp1.length !== temp2.length) return false;
+
+    for(let i =0; i<temp1.length; i++) {
+        if(!temp2.includes(temp1[i])) return false;
+    }
+
+    temp1.map(k => {
+        if(!(['start', 'end', 'comments', 'line', 'column'].includes(k))) {
+            if(!(k in obj2)) return false;
+            if(typeof obj1[k] === 'object') {
+                if(Array.isArray(obj1[k])) {
+                    if(obj1[k].length !== obj2[k].length) return false;
+                    for(let i = 0; i<obj1[k].length; i++) {
+                        if(!objDeepEquals(obj1[k][i], obj2[k][i])) return false;
+                    }
+                } else {
+                    if(!objDeepEquals(obj1[k], obj2[k])) return false;
+                }
+            } else {
+                if(obj1[k] !== obj2[k]) return false;
+            }
+
+        }
+    })
+
+    return true
+}
+
+// static change analysis on tests
+function getTestChangeAncestors(AST1, AST2) {
+    let resArr = [];
+    
+    // Top level variable declare compare
+    const diffIDs = [];
+
+    const testDiffRecur = function(node1, node2, ancestors) {
+        if(objDeepEquals(node1, node2)) return;
+        let tancestors = Array.from(ancestors)
+        tancestors.push(node2);
+        if(t(node1, "callee.name").safeObject === 'describe' && t(node1, "callee.name").safeObject === 'describe') {
+            node2.arguments[1].body.body.forEach(exp => {
+                // assume no more variable declare and assignments in describe
+                if(exp.expression.type === 'CallExpression') {
+                    if(exp.expression.callee.name === 'describe') {
+                        // TODO
+                    } else if (exp.expression.callee.name === 'it') {
+
+                    } else if (exp.expression.callee.name === 'test') {
+
+                    } else if (['beforeEach', 'afterEach', 'beforeAll', 'afterAll', 'after', 'before'].includes(exp.expression.callee.name) ) {
+
+                    } 
+                }
+            })
+        } else {
+            new Error('comparing obj are not describe object', node1, node2)
+        }
+
+
+
+    }
+
+    if(AST1.type === 'Program' && AST2.type === 'Program') {        
+        AST2.body.map(node => {
+            if(node.type === 'VariableDeclaration') {
+                const id = node.declarations[0].id;
+                // find same id in AST1
+                const node1 = AST1.body.filter(ele => {
+                    return ele.type === 'VariableDeclaration' && objDeepEquals(ele.declarations[0].id, id)
+                })
+                
+                if(node1.length > 0) {
+                    if(!objDeepEquals(node.declarations[0].init, node1[0].declarations[0].init)) {
+                        // add id to diffIDs
+                        if(node1[0].declarations[0].id.type === 'ObjectPattern') {
+                            node1[0].declarations[0].id.properties.forEach(p => {
+                                diffIDs.push(p.key.name)
+                            })
+                        } else if (node1[0].declarations[0].id.type === "Identifier") {
+                            diffIDs.push(node1[0].declarations[0].id.name)
+                        }
+                    }
+                }
+            } else if(node.type === 'ExpressionStatement') {
+                if(node.expression === 'CallExpression' && node.expression.callee.name === 'describe') {
+                    // find test suite in AST1
+                    const node1 = AST1.body.filter(ele => {
+                        ele.type === 'ExpressionStatement' && ele.expression === 'CallExpression' 
+                        && ele.expression.callee.name === 'describe' 
+                        && ele.expression.arguments[0].value === node.expression.arguments[0].value
+                    })
+                    if(node1.length > 0) {
+                        testDiffRecur(node1[0].expression, node.expression, [AST2])
+                    } else {
+                        resArr.push([AST2, node.expression])
+                    }
+                } else {
+                    const idx = AST2.body.findIndex(n => _.isEqual(n, node))
+                    if(!objDeepEquals(AST1.body[idx], AST2.body[idx])) {
+                        resArr.push(AST2);
+                    }
+                }
+            } else {
+                const idx = AST2.body.findIndex(n => _.isEqual(n, node))
+                if(!objDeepEquals(AST1.body[idx], AST2.body[idx])) {
+                    resArr.push(AST2);
+                }
+            }
+        })
+    } else {
+        new Error('root level AST type is not Program!')
+    }
+
+    
+    return resArr
+}
+
 /*
  *
  *  Parsing the diff to get a list of changed files, then apply each diff to codebase. 
@@ -100,12 +233,17 @@ module.exports.getChangesAncestors = function (diff) {
     const codeBase = globalUtil.config.codeBase;
     let allChanges = this.parseAndApplyDiff(codeBase, diff);
     allChanges.forEach(change => {
-        if(path.extname(change.filename) === '.js') {
+        if(change.filename.endsWith('.test.js')) {
+            const beforeAST = ASTgenerator.parse(fs.readFileSync(change.filename, 'utf8'));
+            const afterAST = ASTgenerator.parse(change.content);
+            change.diffAncestors = getTestChangeAncestors(beforeAST, afterAST);
+        } else if(path.extname(change.filename) === '.js') {
             const beforeAST = ASTgenerator.parse(fs.readFileSync(change.filename, 'utf8'));
             const afterAST = ASTgenerator.parse(change.content);
             change.diffAncestors = this.getDiffs(beforeAST, afterAST);
         } 
         change.filename = change.filename.replace(path.resolve(codeBase), "");
+        change.content = undefined;
     })
 
     return allChanges;
--- changeAnalysis/test.js
+++ changeAnalysis/test.js
@@ -24,14 +24,14 @@ console.log('res length', res.length)
 res[0].forEach(ele => {
     console.log('ele length', ele.type)
 })
-const diffFile = fs.readFileSync('./diff-3ebe9-803611.patch', "utf8");
-
+const diffFile = fs.readFileSync('../testSelector/sample/changingTests.patch', "utf8");
+globalUtil.setter({codeBase: '../../uppy'})
 const changes = changeAnalysis.getChangesAncestors(diffFile);
 
 // changes.forEach(c => {
 //     console.log(c.filename)
 // })
 
-globalUtil.setter({callGraphPath: '../tmp/uppy.log.json'})
-const selected = TestSelector.getReducedTests(changes)
-console.log(selected)
+// globalUtil.setter({callGraphPath: '../tmp/uppy.log.json'})
+// const selected = TestSelector.getReducedTests(changes)
+// console.log(selected)
