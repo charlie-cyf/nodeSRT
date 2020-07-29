22bf91e7bc18ad882a20cb57d9c57feda969cf49
charlie-cyf <2013oct20@gmail.com>
2020-07-29 15:11:41 -0700

    sync

--- e2eTestsHandler/test.js
+++ e2eTestsHandler/test.js
@@ -2,21 +2,21 @@ const e2eHandler = require('./index');
 const globalUtil = require('../util');
 
 const suite = [
-    // 'chaos-monkey',
-    'create-react-app',
-    'i18n-drag-drop',
-    'thumbnails',
-    'transloadit',
-    'tus-drag-drop',
-    'typescript',
-    'url-plugin',
-    'xhr-limit'
+    'chaos-monkey',
+    // 'create-react-app',
+    // 'i18n-drag-drop',
+    // 'thumbnails',
+    // 'transloadit',
+    // 'tus-drag-drop',
+    // 'typescript',
+    // 'url-plugin',
+    // 'xhr-limit'
   ];
 
 (async function(){
     globalUtil.setter({
         codeBase: '../uppy',
-        E2EprerunInstr: "cd .. && xvfb-run java -Dwebdriver.chrome.driver=/usr/bin/chromedriver -jar selenium-server-standalone-*.jar &",
+        E2EprerunInstr: "cd .. && java -Dwebdriver.chrome.driver=/usr/bin/chromedriver -jar selenium-server-standalone-*.jar &",
         E2EpostrunInstr: 'pkill -9 java',
         buildE2EInstr: 'npm run test:endtoend:build',
         runE2EInstr: 'npx wdio test/endtoend/wdio.local.conf.js "-b" "chrome" "--suite" "${suitename}"'
